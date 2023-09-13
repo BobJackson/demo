4c26eccdcf888ceaab78f0ffcecd5577225b9b89
Bob Jackson <wangyousong@9191offer.com>
2023-09-13 22:23:06 +0800

    finnish      //TODO:  multi options: -l -p 8080 -d /usr/logs

--- src/test/java/com/example/demo/ArgsTest.java
+++ src/test/java/com/example/demo/ArgsTest.java
@@ -28,7 +28,6 @@ public class ArgsTest {
 
     }
 
-
     @Test
     void should_parse_int_as_option_value() {
         IntOption option = Args.parse(IntOption.class, "-p", "8080");
@@ -38,10 +37,6 @@ public class ArgsTest {
     static record IntOption(@Option("p") int port) {
 
     }
-
-
-    //TODO:  - String    -d /usr/logs
-
 
     @Test
     void should_get_string_as_option_value() {
@@ -53,30 +48,31 @@ public class ArgsTest {
 
     }
 
-    //TODO:  multi options: -l -p 8080 -d /usr/logs
-
-    // sad path:
-    //TODO: -bool -l t / -l t f
-    //TODO: - int -p / -p 8080 8081
-    //TODO: - string -d / -d /usr/logs /usr/vars
-
-    // default value
-    //TODO: - bool : false
-    //TODO: - int : 0
-    //TODO: - string : ""
-
-
     @Test
-    @Disabled
-    void should_example_1() {
-        Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
+    void should_parse_multi_options() {
+        MultiOptions options = Args.parse(MultiOptions.class, "-l", "-p", "8080", "-d", "/usr/logs");
         assertTrue(options.logging());
         assertEquals(8080, options.port());
         assertEquals("/usr/logs", options.directory());
 
     }
+    static record MultiOptions(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
+    }
+
+    // sad path:
+    //TODO: -bool -l t / -l t f
+
+    //TODO: - int -p / -p 8080 8081
+    //TODO: - string -d / -d /usr/logs /usr/vars
+    // default value
+    //TODO: - bool : false
+
+    //TODO: - int : 0
+
+    //TODO: - string : ""
 
     // -g this is a list -d 1 2 -3 5
+
     @Test
     @Disabled
     void should_example_2() {
@@ -85,9 +81,6 @@ public class ArgsTest {
         assertArrayEquals(new String[]{"this", "is", "a", "list"}, options.group());
         assertArrayEquals(new int[]{1, 2, -3, 5}, options.decimals());
 
-    }
-
-    static record Options(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
     }
 
     static record ListOptions(@Option("g") String[] group, @Option("d") int[] decimals) {
