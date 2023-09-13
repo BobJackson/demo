e61a8ab87c10f2382b17875dcab68c55576b415a
Bob Jackson <wangyousong@9191offer.com>
2023-09-13 23:39:07 +0800

    refactor：通过函数式接口参数完成重复代码的移除，本质上是把职责委托到了函数式字段，进一步消除重复代码，当然可能要处理一下类型，向上转，获得更多的自由，同时可以inline掉parseValue()

--- src/main/java/com/example/demo/IntOptionParser.java
+++ src/main/java/com/example/demo/IntOptionParser.java
@@ -18,10 +18,6 @@ class IntOptionParser implements OptionsParser {
     public Object parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
         String value = arguments.get(index + 1);
-        return parseValue(value);
-    }
-
-    protected Object parseValue(String value) {
         return valueParser.apply(value);
     }
 
