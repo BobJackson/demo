b04240157f93dc9635a9669667fc640f0e2822b0
Bob Jackson <wangyousong@9191offer.com>
2023-09-15 00:16:48 +0800

    refactor: inline

--- src/main/java/com/example/demo/SingularValueOptionParser.java
+++ src/main/java/com/example/demo/SingularValueOptionParser.java
@@ -21,14 +21,14 @@ class SingularValueOptionParser<T> implements OptionsParser<T> {
         List<String> values = getValues(arguments, index);
         if (values.isEmpty()) throw new InsufficientArgumentsException(option.value());
         if (values.size() > 1) throw new TooManyArgumentsException(option.value());
-        return valueParser.apply(arguments.get(index + 1));
+        String value = values.get(0);
+        return valueParser.apply(value);
     }
 
     private static List<String> getValues(List<String> arguments, int index) {
-        int followingFlag = IntStream.range(index + 1, arguments.size())
+        return arguments.subList(index + 1, IntStream.range(index + 1, arguments.size())
                 .filter(it -> arguments.get(it).startsWith("-"))
-                .findFirst().orElse(arguments.size());
-        return arguments.subList(index + 1, followingFlag);
+                .findFirst().orElse(arguments.size()));
     }
 
 }
