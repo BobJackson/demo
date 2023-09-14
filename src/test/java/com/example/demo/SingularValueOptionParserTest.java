1be8a13bf2ff908714c27de7fcd3703c4ca0f797
Bob Jackson <wangyousong@9191offer.com>
2023-09-14 23:27:41 +0800

    为了让测试更通用用了whatever 和 Object

--- src/test/java/com/example/demo/SingularValueOptionParserTest.java
+++ src/test/java/com/example/demo/SingularValueOptionParserTest.java
@@ -5,44 +5,48 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.function.Function;
 
 import static com.example.demo.BooleanParserTest.option;
 import static java.util.Arrays.asList;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SingularValueOptionParserTest {
 
     @Test
+        // sad path
     void should_not_accept_extra_argument_for_singular_valued_option() {
         TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> {
-            new SingularValueOptionParser<Integer>(0, Integer::parseInt).parse(asList("-p", "8080", "8081"), option("p"));
+            new SingularValueOptionParser<>(0, Integer::parseInt).parse(asList("-p", "8080", "8081"), option("p"));
         });
 
         assertEquals("p", e.getOption());
     }
 
-    @ParameterizedTest
+    @ParameterizedTest // sad path
     @ValueSource(strings = {"-p -l", "-p"})
     void should_not_accept_insufficient_argument_for_singular_valued_option(String arguments) {
         InsufficientArgumentsException e = assertThrows(InsufficientArgumentsException.class, () -> {
-            new SingularValueOptionParser<Integer>(0, Integer::parseInt).parse(asList(arguments.split(" ")), option("p"));
+            new SingularValueOptionParser<>(0, Integer::parseInt).parse(asList(arguments.split(" ")), option("p"));
         });
 
         assertEquals("p", e.getOption());
     }
 
     @Test
-    void should_set_default_value_to_0_for_int_option() {
-        assertEquals(0, new SingularValueOptionParser<Integer>(0, Integer::parseInt).parse(List.of(), option("p")));
+        // default value
+    void should_set_default_value_for_singular_valued_option() {
+        Function<String, Object> whatever = (it) -> null;
+        Object defaultValue = new Object();
+        assertEquals(defaultValue, new SingularValueOptionParser<>(defaultValue, whatever).parse(List.of(), option("p")));
     }
 
     @Test
-    void should_not_accept_extra_argument_for_string_singular_valued_option() {
-        TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> {
-            new SingularValueOptionParser<>("", String::valueOf).parse(asList("-d", "/usr/logs", "/usr/vars"), option("d"));
-        });
-
-        assertEquals("d", e.getOption());
+        // happy path
+    void should_parse_value_if_flag_present() {
+        Object parsed = new Object();
+        Function<String, Object> parse = (it) -> parsed;
+        Object whatever = new Object();
+        assertSame(parsed, new SingularValueOptionParser<>(whatever, parse).parse(List.of("-p", "8080"), option("p")));
     }
 }
