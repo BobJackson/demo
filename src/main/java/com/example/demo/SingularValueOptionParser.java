0bd240190fb4f7bc318d4562e074a8f86c80f642
Bob Jackson <wangyousong@9191offer.com>
2023-09-15 00:13:06 +0800

    refactor: 就算是抽取方法也只会让代码好上一丢丢，这时候说明应该有更好的方式来实现它，让它更加直观

--- src/main/java/com/example/demo/SingularValueOptionParser.java
+++ src/main/java/com/example/demo/SingularValueOptionParser.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 class SingularValueOptionParser<T> implements OptionsParser<T> {
 
@@ -17,11 +18,17 @@ class SingularValueOptionParser<T> implements OptionsParser<T> {
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
         if (index == -1) return defaultValue;
-        if (index + 1 == arguments.size() ||
-                arguments.get(index + 1).startsWith("-")) throw new InsufficientArgumentsException(option.value());
-        if (index + 2 < arguments.size() &&
-                !arguments.get(index + 2).startsWith("-")) throw new TooManyArgumentsException(option.value());
+        List<String> values = getValues(arguments, index);
+        if (values.size() < 1) throw new InsufficientArgumentsException(option.value());
+        if (values.size() > 1) throw new TooManyArgumentsException(option.value());
         return valueParser.apply(arguments.get(index + 1));
+    }
+
+    private static List<String> getValues(List<String> arguments, int index) {
+        int followingFlag = IntStream.range(index + 1, arguments.size())
+                .filter(it -> arguments.get(it).startsWith("-"))
+                .findFirst().orElse(arguments.size());
+        return arguments.subList(index + 1, followingFlag);
     }
 
 }
